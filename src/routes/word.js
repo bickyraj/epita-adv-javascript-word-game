e54fc6eb9af3c6294403732cac1f035638d9e023
Bicky Raj Kayastha <bickyrajkayastha@gmail.com>
2023-07-07 11:34:21 +0200

    updates

--- src/routes/word.js
+++ src/routes/word.js
@@ -4,25 +4,24 @@ const Router = express.Router();
 const WordModel = require('../models/word');
 
 Router.post('/', async (request, response) => {
-    const { word } = request.body;
+  const { word } = request.body;
 
-    const wordModel = new WordModel({ 
-        name: word
+  const wordModel = new WordModel({
+    name: word
+  });
+
+  try {
+
+    await wordModel.save();
+    return response.status(200).json({
+      "msg": word
     });
 
-    try {
-
-        await wordModel.save();
-
-        return response.status(200).json({
-            "msg": word
-        });
-
-    } catch (error) {
-        return response.status(500).json({
-            "error": error.message
-        });
-    }
+  } catch (error) {
+    return response.status(500).json({
+      "error": error.message
+    });
+  }
 });
 
 module.exports = Router;
