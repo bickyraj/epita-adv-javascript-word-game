6e7d18a1425f0997246a0e9851b25c5f72e48200
Bicky Raj Kayastha <bickyrajkayastha@gmail.com>
2023-07-16 18:31:38 +0200

    udpates on playing the game and giving the response

--- src/routes/game.js
+++ src/routes/game.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const WordModel = require('../models/word');
 const GameModel = require("../models/game");
+const TryModel = require("../models/try");
 const { compareAlphabetsBetweenWords, jsonResponse } = require('../utils');
 
 const Router = express.Router();
 
 const isLogged = (request, response, next) => {
-    if (request.session.user) {
-        next();
-    } else {
-        return response.status(500).json({'msg': "not logged !"})
-    }
+  if (request.session.user) {
+    next();
+  } else {
+    return response.status(500).json({ 'msg': "not logged !" })
+  }
 }
 
 // create a game
@@ -36,88 +37,62 @@ Router.get('/new', isLogged, async (request, response) => {
 
 Router.post('/try', async (request, response) => {
   // get a random words from the words.
-  const { id, word } = request.body; 
-
-  const wordModel = await WordModel.findById(id);
-  console.log(wordModel);
+  const { word_id, game_id, word } = request.body;
+  const wordModel = await WordModel.findById(word_id);
+  const gameModel = await GameModel.findById(game_id);
 
   const result = compareAlphabetsBetweenWords(wordModel.name, word);
+  let newTry = await new TryModel({
+    word: word_id,
+    result: result
+  }).save();
 
-  return response.status(200).json({
-    "word": word,
-    "response": result,
-  })
+  gameModel.tries.push(newTry);
+
+  try {
+    const updatedGame = await gameModel.save();
+    return response.status(200).json({
+      "word": word,
+      "response": result,
+      "game": updatedGame
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-// Router.post('/', async (request, response) => {
-//     const word = await WordModel.aggregate([{
-//         $sample: {size: 1}
-//     }]);
-
-//     let game = new GameModel({
-//         word: word[0]._id,
-//         tries: [],
-//         user: request.session.user._id
-//     });
-
-//     try {
-//         await game.save();
-
-//         game = await GameModel.find({
-//             _id: game._id
-//         }).populate('user').populate('word')
-
-//         return response.status(200).json({
-//             "msg": game
-//         });
-//     } catch (error) {
-//         return response.status(500).json({
-//             "error": error.message
-//         });
-//     }
-// });
-
 Router.get('/:id', async (request, response) => {
-    const {id} = request.params;
+  const { id } = request.params;
 
-    try {
-        const game = await GameModel.findOne({_id: id});
+  try {
+    const game = await GameModel.findOne({ _id: id });
 
-        return response.status(200).json({
-            "msg": game
-        });
-    } catch (error) {
-        return response.status(500).json({
-            "error": error.message
-        });
-    }
+    return response.status(200).json({
+      "msg": game
+    });
+  } catch (error) {
+    return response.status(500).json({
+      "error": error.message
+    });
+  }
 })
 
 Router.post('/verif', isLogged, (request, response) => {
-    // get the value from the user
-
-    // ge the value searched by getting the game
-
-    // make the verification
-
-    // send the result
-
-
-    if (typeof request.body.word === 'undefined') {
-        return response.status(500).json({
-            "msg": "You have to send 'word' value"
-        });
-    }
-
-    if (request.body.word === search) {
-        return response.status(200).json({
-            "result": "You find the word !"
-        });
-    }
-
+  if (typeof request.body.word === 'undefined') {
     return response.status(500).json({
-        "result": "You don't find the word !"
+      "msg": "You have to send 'word' value"
     });
+  }
+
+  if (request.body.word === search) {
+    return response.status(200).json({
+      "result": "You find the word !"
+    });
+  }
+
+  return response.status(500).json({
+    "result": "You don't find the word !"
+  });
 })
 
 module.exports = Router;
